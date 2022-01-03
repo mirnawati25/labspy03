6f51961172608c2c8f41e328a98fbf16d5ac472a
Mirnawati <mw151476@gmail.com>
2022-01-03 18:59:46 +0700

    first commit

---
+++ latihan2.py
@@ -0,0 +1,19 @@
+a = range(80, 100)
+b = range(79, 79)
+c = range(60, 69)
+d = range(50, 59)
+e = range(0, 49)
+
+nilai = [a, b, c, d, e]
+while True:
+    if nilai == a:
+        print('Nilai : A')
+    elif nilai == b:
+        print('Nilai : B')
+    elif nilai == c:
+        print('Nilai : C')
+    elif nilai == d:
+        print('Nilai : D')
+    else:
+        print('Nilai : E')
+    break
